939b6165e7f825febdf93e29ee5a1b75bfc0fedb
Thefirush <zefirush@icloud.com>
2021-09-29 10:50:10 +0300

    .

---
+++ src/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int a = sc.nextInt();
+    int b = sc.nextInt();
+    System.out.println("a = " + a +" and " + "b = " + b);
+    int c = a;
+    a = b;
+    b = c;
+    System.out.println("a = " + a + " and " + "b = " + b);
+
+    }
+}
